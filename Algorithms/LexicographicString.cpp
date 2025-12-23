5355154e0af6d13744efed786798e64db0a2c469
harleen001 <harleen.johal31@gmail.com>
2025-12-23 18:19:36 +0530

    lexico

--- Algorithms/LexicographicString.cpp
+++ Algorithms/LexicographicString.cpp
@@ -1,31 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void sortlexico(char &fruit)
-{
-    int n=fruit.size();
-    for(int i=0;i<n-1;i++)
-    {
-     //finding minimum element
-      int min_index=i;
-      for(int j=i+1;j<n;j++)
-      {
-      if(strcmp (fruit[j],fruit[min_index])>0)
-      {
-        min_index=j;
-      }
+// Pass a vector of strings by reference
+void sortLexico(vector<string> &fruit) {
+    int n = fruit.size();
+    for (int i = 0; i < n - 1; i++) {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++) {
+            // Lexicographical comparison (A-Z)
+            if (fruit[j] < fruit[min_index]) {
+                min_index = j;
+            }
+        }
+        if (i != min_index) {
+            swap(fruit[i], fruit[min_index]);
+        }
     }
-     //place minimum at begining of unsorted array
-     if(i!=min_index)
-     {
- swap(fruit[i],fruit[min_index]);
-     }
-    
-}
 }
 
-int main()
-{
-    char fruit[][60]={"papaya","lime","watermelon","apple","mango","kiwi"};
+int main() {
+    vector<string> fruit = {"papaya", "lime", "watermelon", "apple", "mango", "kiwi"};
+    
+    sortLexico(fruit);
+
+    cout << "Sorted Fruits: " << endl;
+    for (const string &f : fruit) {
+        cout << f << " ";
+    }
     return 0;
 }
